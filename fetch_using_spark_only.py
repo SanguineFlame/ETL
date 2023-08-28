14f2a9b266ab645464de8bab271d297443adaeb9
SanguineFlame <35411577+SanguineFlame@users.noreply.github.com>
2023-08-28 19:40:10 -0400

    Adding renaming capabilities to script.

--- fetch_using_spark_only.py
+++ fetch_using_spark_only.py
@@ -7,8 +7,8 @@ df = spark.read \
     .format("jdbc") \
     .option("url", "jdbc:mysql://database-1.c4iwusdi8hal.us-east-1.rds.amazonaws.com:3306/chrome_schema") \
     .option("dbtable", "myTable") \
-    .option("user", "x") \
-    .option("password", "x") \
+    .option("user", "z") \
+    .option("password", "z") \
     .load()
 
 df.createOrReplaceTempView("my_table_view")
@@ -26,10 +26,11 @@ desired_filename = 'my_output_file.csv'
 # Initialize the S3 client
 s3_client = boto3.client('s3')
 bucket_name = 'testing-thangs-123'
-prefix = 'incoming/feed1/'
+incoming_prefix = 'incoming/feed1/'
+
 
 # List the objects in the specified S3 directory
-objects = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+objects = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=incoming_prefix)
 
 # There should only be one file in this directory (the 'part-' file)
 obj_key = objects.get('Contents', [{}])[0].get('Key')
@@ -37,5 +38,5 @@ obj_key = objects.get('Contents', [{}])[0].get('Key')
 if obj_key:
     source = {'Bucket': bucket_name, 'Key': obj_key}
     # Rename (actually copy to new name and then delete the original)
-    s3_client.copy_object(Bucket=bucket_name, CopySource=source, Key=prefix + desired_filename)
+    s3_client.copy_object(Bucket=bucket_name, CopySource=source, Key=incoming_prefix + desired_filename)
     s3_client.delete_object(Bucket=bucket_name, Key=obj_key)
