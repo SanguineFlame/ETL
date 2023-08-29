b9902bee856a34cfa2b8d5329f4cbc4e030d3cc8
SanguineFlame <35411577+SanguineFlame@users.noreply.github.com>
2023-08-29 10:47:44 -0400

    Update fetch_using_spark_only.py
    Adding Secret Manager capabilities.

--- fetch_using_spark_only.py
+++ fetch_using_spark_only.py
@@ -1,5 +1,36 @@
+import json
 from pyspark.sql import SparkSession
 import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+    secret_name = "dev/secret/mySQL"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = json.loads(get_secret_value_response['SecretString'])
+
+    return secret
+
+
+secrets = get_secret()
 
 spark = SparkSession.builder.appName("SparkSQLExample").getOrCreate()
 
@@ -7,17 +38,15 @@ df = spark.read \
     .format("jdbc") \
     .option("url", "jdbc:mysql://database-1.c4iwusdi8hal.us-east-1.rds.amazonaws.com:3306/chrome_schema") \
     .option("dbtable", "myTable") \
-    .option("user", "z") \
-    .option("password", "z") \
+    .option("user", secrets["username"]) \
+    .option("password", secrets["password"]) \
     .load()
 
 df.createOrReplaceTempView("my_table_view")
 
 result = spark.sql("SELECT * FROM my_table_view;")
 
-
 result.coalesce(1).write.option("delimiter", "|").mode("overwrite").csv("s3://testing-thangs-123/incoming/feed1/")
-
 
 # Assuming you've already written the DataFrame to a unique S3 directory using Spark with coalesce(1)
 unique_output_directory = 's3://testing-thangs-123/incoming/feed1/'
@@ -27,7 +56,6 @@ desired_filename = 'my_output_file.csv'
 s3_client = boto3.client('s3')
 bucket_name = 'testing-thangs-123'
 incoming_prefix = 'incoming/feed1/'
-
 
 # List the objects in the specified S3 directory
 objects = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=incoming_prefix)
